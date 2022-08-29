b828ae52181a73a5ce40bfc6ddc30b787aad3fca
JosemaCeballos <josema.soyhenry@gmail.com>
2022-08-29 16:35:25 -0300

    Resolución de la homework 01-Intro to CS

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,12 +2,21 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  var array = num.split('');
+  var sum = 0;
 
+  for (var i=0; i<array.length; i++){
+    sum = sum + 2**(array.length -i -1) * array[i]
+  }
+
+  return sum;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  if (num === 0) return '0';
+  if (num === 1) return '1';
+  return DecimalABinario(Math.floor(num/2)) + (num%2)
 }
 
 
